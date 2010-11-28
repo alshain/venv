e6a0c577c11d0845b93e1af7e9615c0c9e73eca7
alshain <c.vonrueti@project-infcom.ch>
2010-11-28 16:52:19 +0100

    WORKS :)

--- __init__.py
+++ __init__.py
@@ -1,12 +1,23 @@
-import sys
+import sys, imp, os
+
+class vEnvError(RuntimeError):
+    pass
 
 class vEnv(object):
     def __init__(self):
+        self._sourceDir = None
         self._mappings = []
+        self._next_file = None
 
-    def map(self, virtual, actual):
+    def setSourceDir(self, dir):
+        if os.path.isdir(dir):
+            self._sourceDir = dir
+        else:
+            raise vEnvError("Source directory does not exist.")
+
+    def map(self, virtual, path):
         """Redirect a module import."""
-        self._mappings.insert(0, (virtual, actual))
+        self._mappings.insert(0, (virtual, path))
         self._sort()
 
     def _sort(self):
@@ -37,24 +48,78 @@ class vEnv(object):
 
     def find_module(self, module_name, package_path):
         """PEP 302: Module Loader"""
-        for virtual, actual in self._mappings:
+        if not self._sourceDir:
+            raise vEnvError("Source dir not set.")
+
+        for virtual, path in self._mappings:
             if module_name.startswith(virtual):
-                print module_name
-                print package_path
-                return self
+                partial = module_name[len(virtual) + 1:]
+                try:
+                    if partial:
+                        return self._resolve_import(path, partial.split("."))
+                    else:
+                        return self._resolve_import(path)
+                except vEnvError as e:
+                    print e
+                    return None
         return
+    def _resolve_import(self, path, partials = None):
+        if partials:
+            submodules = partials[:-1]
+            path = os.path.join(self._sourceDir, path, *submodules)
+            if not os.path.isdir(path):
+                raise vEnvError("No such path: %s" % path)
+            package = os.path.join(path, partials[-1], '__init__.py')
+            module = os.path.join(path, partials[-1]) + '.py'
+            if os.path.isfile(package):
+                self._next_file = package
+            elif os.path.isfile(module):
+                self._next_file = module
+            else:
+                return None
+        else:
+            # Import entire module
+            # Look for __init__.py
+            init = os.path.join(self._sourceDir, path, '__init__.py')
+            if os.path.isfile(init):
+                self._next_file = init
+            else:
+                raise vEnvError("__init__.py not found in %s" % os.path.dirname(path))
+        return self
 
     def load_module(self, fullname):
-        print "Attempting to load: %s" % fullname
-        pass
+        # According to PEP 302:
+        # If there is an existing module object named 'fullname' in
+        # sys.modules, the loader must use that existing module.
+        # (Otherwise, the reload() builtin will not work correctly.)
+        # According to PEP 302:
+        # If a module named 'fullname' does not exist in sys.modules,
+        # the loader must create a new module object and add it to
+        # sys.modules.
+        if self._next_file:
+            ispkg = self._next_file.endswith('__init__.py')
+            if fullname in sys.modules:
+                return sys.modules[fullname]
+            # Load module because not loaded
+            with open(self._next_file, 'U') as f:
+                if ispkg:
+                    description = ("", "", imp.PKG_DIRECTORY)
+                else:
+                    description = ("", "", imp.PY_SOURCE)
+                mod = imp.load_module(fullname, f, self._next_file, description)
+                sys.modules[fullname] = mod
+                exec f in mod.__dict__
+            return mod
+        else: raise vEnvError("Invalid next file.")
 
-
-
+# Initialize vEnv instance
 vEnv = vEnv()
-
 
 def map(virtual, actual):
     """Redirect import."""
     vEnv.map(virtual, actual)
+
+def set_source_dir(dir):
+    vEnv.setSourceDir(dir)
 
 sys.meta_path.insert(0, vEnv)
